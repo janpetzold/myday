c96ba3718b352bea68c6001c5cc28c2875288e3b
Jan Petzold <janpetzold@outlook.com>
2024-04-05 16:56:37 +0300

    Adding new rule for Cloudflare to handle CSS

--- cloudflare/worker.js
+++ cloudflare/worker.js
@@ -1,15 +1,20 @@
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
-  })
-  
-  async function handleRequest(request) {
-    const url = new URL(request.url)
-    if (url.pathname.includes('api')) {
-      // API calls to Azure function
-      const newURL = `https://myday.azurewebsites.net${url.pathname}`
-      return fetch(newURL, request)
-    } else {
-      // All other calls to website
-      return fetch("https://janpetzold.github.io/myday/", request)
-    }
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+  if (url.pathname.includes('api')) {
+    // Route API calls to Azure function
+    const newURL = `https://myday.azurewebsites.net${url.pathname}`
+    return fetch(newURL, request)
+  } else if(url.pathname.includes(".css")) {
+    // Unpretty special rule for Github Pages CSS
+    const newURL = `https://janpetzold.github.io/${url.pathname}`
+    return fetch(newURL, request)
+  } else {
+    // Route to website in case the URL is entered
+    const newURL = `https://janpetzold.github.io/myday${url.pathname}`
+    return fetch(newURL, request)
   }
+}
